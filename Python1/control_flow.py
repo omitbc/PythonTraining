e8d99c9143d84471e5236e15bb65966422093128
omitbc <bajracharyaomit@gmail.com>
2023-06-25 23:17:32 +0545

    Initial Commit..

---
+++ Python1/control_flow.py
@@ -0,0 +1,120 @@
+#Single Line Comment
+
+'''
+Multiple line
+Comment
+'''
+
+# n=20
+# if n%2==0:
+#     print("even no")
+# else:
+#     print("odd")    
+
+a=10
+# a < 1 and print('this print statement is executed')
+# a > 3 and print('this print statement is not executed')
+
+temp = 2
+# a > 1 and temp != 0  and print(a // temp)
+
+# if temp!=0:
+#     if a>1:
+#         print(a//temp)
+
+a=1
+# #using or
+# a == 2 or print('this will not get executed')
+# a == 1 or print('this will get executed')
+
+# AND 
+# A B (A and B)
+# T T  True
+# T F  False
+# F T  False
+# F F  false
+
+
+# '''
+# if True {
+#     print('this is in other programming languages like c')
+# }
+
+# '''
+
+# '''
+# if False:
+#     print('False')
+#     print('False')
+#     print('False')
+
+# if True:
+#     print('True')
+#     print('True')
+#     print('True')
+# '''
+
+# a = 10
+# b = 5
+
+# #alternate execution
+# if a > b : #false
+#     print('it is true')
+# else:
+#     print('not true ')
+
+
+# a=6
+# b=7
+# #chained condition
+# if a == 3 : #flase
+#     print('it is true')
+# elif a == b: 
+#     print('both are equal')
+# elif b == 5:
+#     print(' b is 5')
+# elif b==7:
+#     print('hit')    
+# else:
+#     print('none')
+
+# #nested condition
+# if a == b:
+#     print('equal')
+# else:
+#     if a > b:
+#         print('a is greater than b')
+#     else:
+#         print('b is greater than a')
+
+
+
+a=10
+b=12
+c=13
+if False or True or True:
+    print("success")
+else:
+    print("not success")
+   
+
+
+
+
+'''
+#Q) wap to find the greatest no among two(a and b).
+#Take two input from user store in variable eg. a and b
+#Check if: 
+# a is greater
+# b is greater
+# a and b both are equal  
+'''
+# a=input("Enter 1st no:")
+# b=input("Enter second no:")
+# if a==b:
+#     print("a and b both are equal")
+# elif a>b:
+#     print("a is greater than b")
+# else:
+#     print("b is greater than a")
+
---
+++ Python1/operators.py
@@ -0,0 +1,34 @@
+# This file contains basic python operators
+
+# Addition
+print("Addition:", 6+10)
+
+# Subraction
+print("Subraction:", 5-7)
+
+# Multiplication
+print("Multplication:", 5*10)
+
+# Division
+print("Division:", 9/2)
+
+# Floor Division
+print("Floor Division:", 9//2) 
+
+# Modulo Division
+print("Modulo Division:", 5%2)
+
+# Exponent
+print("Exponent:", 2**16)
+
+
+# Comparision
+print("Equality:", 1==1)
+print("Inequality:", 1!=1)
+print("Greater Than:", 2>1)
+print("Greater Than Equals To:", 6>=5)
+print("Less Than:", 1<2)
+print("Less Than Equals To:", 5<=10)
+
+#assignment
+x=3
